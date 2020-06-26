7673567df4985b496c3ce24c579e39a069a38513
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-26 05:51:57 +0000

    Deploy plantain-00/image-pixel-compare to github.com/plantain-00/image-pixel-compare.git:gh-pages

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -2,7 +2,6 @@ import { executeScriptAsync } from 'clean-scripts'
 import { watch } from 'watch-then-execute'
 
 const tsFiles = `"*.ts"`
-const jsFiles = `"*.config.js"`
 const lessFiles = `"*.less"`
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -34,7 +33,7 @@ export default {
     swCommand
   ],
   lint: {
-    ts: `eslint --ext .js,.ts,.tsx ${tsFiles} ${jsFiles}`,
+    ts: `eslint --ext .js,.ts,.tsx ${tsFiles}`,
     less: `stylelint ${lessFiles}`,
     export: `no-unused-export ${tsFiles} ${lessFiles}`,
     markdown: `markdownlint README.md`,
@@ -43,7 +42,7 @@ export default {
   test: [
   ],
   fix: {
-    ts: `eslint --ext .js,.ts,.tsx ${tsFiles} ${jsFiles} --fix`,
+    ts: `eslint --ext .js,.ts,.tsx ${tsFiles} --fix`,
     less: `stylelint --fix ${lessFiles}`
   },
   watch: {
